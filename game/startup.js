d823e5801f1874bf4e6bf947d27847b0bde3b589
tom.brewster <tom.brewster@parashift.com.au>
2017-11-02 17:00:22 +1030

    Blurble

--- game/startup.js
+++ game/startup.js
@@ -1,4 +1,14 @@
-var wood = 0;
-var stone = 0;
-var food = 0;
-var crystal = 0;
+var Startup = (function() {
+
+  instance.start = function() {
+    var wood = 0;
+    var stone = 0;
+    var food = 0;
+    var crystal = 0;
+  };
+
+}());
+
+window.onload = function(){
+    Startup.start();
+};
