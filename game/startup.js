438ab13d05ef34cf1e4c981e947f37a2d4043ddc
tom.brewster <tom.brewster@parashift.com.au>
2017-11-03 10:28:02 +1030

    Try to get wood (heh)

--- game/startup.js
+++ game/startup.js
@@ -1,15 +1,1 @@
-function incremental()
-{
-	this.wood=0;
-
-	this.addWood = funtion(x)
-	{
-		this.wood+=x;
-	}
-}
-
-window.onload = function(){
-
-var Game = new incremental();
-
-};
+var wood = 0;
