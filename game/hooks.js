7dd5d33705be6b43263f9d290fae2160b536de0a
tom.brewster <tom.brewster@parashift.com.au>
2017-11-03 09:38:48 +1030

    Hooks

---
+++ game/hooks.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+    document.getElementById('woodAdd').onclick = function() {addWood()};
+
+    function addWood() {
+        console.log(resources[wood]);
+    }
+    
+};
