57161f044af923879af197b89f377cf70ae5f946
tom.brewster <tom.brewster@parashift.com.au>
2017-11-03 11:36:59 +1030

    Shake things up a little.

--- game/hooks.js
+++ game/hooks.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
     for(var i = 0; i < tier1.length; i++) {
-        document.getElementById(tier1[i]+'Add').addEventListener('click', function() {addResource(tier1[i],1)}, false);
+        var name = tier1[i];
+        document.getElementById(name+'Add').addEventListener('click', function() {addResource(name,1)}, false);
     }
 });
 function addResource(x,y) {
-    return window[x] += y;
+   return window[x] += y;
 };
