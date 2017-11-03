79735354ad6bf977d32328520d0c65ec68c4b502
tom.brewster <tom.brewster@parashift.com.au>
2017-11-03 11:40:31 +1030

    ?

--- game/hooks.js
+++ game/hooks.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     for(var i = 0; i < tier1.length; i++) {
         var name = tier1[i];
-        document.getElementById(name+'Add').addEventListener('click', function() {addResource(name,1)}, false);
+        document.getElementById(name+'Add').addEventListener('click', function() {addResource(tier1[i],1)}, false);
     }
 });
 function addResource(x,y) {
