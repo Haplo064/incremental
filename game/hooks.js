a405df2fa55f4d0fbc4c94cf54c9db03923ba70a
tom.brewster <tom.brewster@parashift.com.au>
2017-11-03 10:44:04 +1030

    Extracted

--- game/hooks.js
+++ game/hooks.js
@@ -1,3 +1,8 @@
 window.onload = function(){
-    document.getElementById('woodAdd').onclick = wood++;
+    document.getElementById('woodAdd').onclick = addWood(1);
 };
+function addWood(x) {
+    console.log(wood);
+    console.log(x);
+    return wood += x;
+}
