704a1295bc6ef2a3b051bbfd5189517699f77d98
tom.brewster <tom.brewster@parashift.com.au>
2017-11-03 11:28:30 +1030

    More breakls.

--- game/hooks.js
+++ game/hooks.js
@@ -1,7 +1,21 @@
 $(document).ready(function() {
-    console.log("Poop");
     document.getElementById('woodAdd').addEventListener('click', function() {addWood(1)}, false);
+    document.getElementById('stoneAdd').addEventListener('click', function() {addStone(1)}, false);
+    document.getElementById('foodAdd').addEventListener('click', function() {addFood(1)}, false);
+    document.getElementById('crystalAdd').addEventListener('click', function() {addCrystal(1)}, false);
 });
 function addWood(x) {
     return wood += x;
+}
+
+function addStone(x) {
+    return stone += x;
+}
+
+function addFood(x) {
+    return food += x;
+}
+
+function addCrystal(x) {
+    return crystal += x;
 }
